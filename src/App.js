bd2a1f39a51e3a3ebc5e0839445234873fc61c7f
Developer <developer@Rudys-Mac-mini.local>
2023-04-11 13:29:25 -0700

    routing update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import logo from './NexusHomeRep.svg';
 import './App.css';
-import { Outlet, Link } from "react-router-dom";
+import About from './routes/About';
+import Home from './routes/Home';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       
-      <h6>TEST</h6>
-
        {/* <Link to="about">About Us</Link> */}
      
-      <div id="detail">
-        <Outlet />
-      </div>
+       <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+       
+            <Link to="/">Home</Link>
+       
+            <Link to="/about">About</Link>
+         
+            <Link to="/nothing-here">Nothing Here</Link>
+       
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
+
--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
 import React from 'react';
 import * as ReactDOM from "react-dom";
-import About from './routes/About';
-import Home from './routes/Home';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {BrowserRouter,} from "react-router-dom";
 import './index.css';
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-  // {
-  //   path: "",
-  //   element: <Home />, // Render Home component as the default component
-  // },
-  {
-    path: "/",
-    element: <App />,
-    
-    children: [
-      {
-        index:  true,
-        path: "Home/",
-        element: <Home />,
-      },
-      {
-        path: "about/",
-        element: <About />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
