a712e4cc8c1f006a2e8c786a448d821c9e178c8f
Developer <developer@Rudys-Mac-mini.local>
2023-04-12 15:52:14 -0700

    services layout

--- src/routes/Services.js
+++ src/routes/Services.js
@@ -3,34 +3,38 @@ export default function Services() {
       <>
         <h1>I Offer the Following Services</h1>
         <div className="services-grid">
-          <div className="service-box">
-            <h2>Electrical  Repair</h2>
-            <p>Installation and repair of lighting fixtures, ceiling fans, wall outlets and switches.</p>
-            {/* Additional information or content for Service 1 */}
-          </div>
-          <div className="service-box">
-            <h2>Service 2</h2>
-            <p>Description of Service 2</p>
-            {/* Additional information or content for Service 2 */}
-          </div>
-          <div className="service-box">
-            <h2>Service 3</h2>
-            <p>Description of Service 3</p>
-            {/* Additional information or content for Service 3 */}
-          </div>
-          <div className="service-box">
-            <h2>Service 4</h2>
-            <p>Description of Service 4</p>
-            {/* Additional information or content for Service 4 */}
-          </div>
-          <div className="service-box">
-            <h2>Service 5</h2>
-            <p>Description of Service 5</p>
+
+        <div className="service-box">
+            <h2>Installations</h2>
+            <p>Wall mounting of TV's, artwork, shelving, and blinds.</p>
             {/* Additional information or content for Service 5 */}
           </div>
           <div className="service-box">
-            <h2>Service 6</h2>
-            <p>Description of Service 6</p>
+            
+            <h2>Electrical  Repair</h2>
+            <p>Installation and repair of lighting fixtures, ceiling fans, wall outlets, and switches.</p>
+            {/* Additional information or content for Service 1 */}
+          </div>
+          <div className="service-box">
+            <h2>Finish Carpentry</h2>
+            <p>Installation of trim, molding, doors, and cabinetry.</p>
+            {/* Additional information or content for Service 2 */}
+          </div>
+          <div className="service-box">
+            <h2>Furniture Assembly</h2>
+            <p>ssembly services for Ikea and other furniture brands, accommodating a wide range of furniture types and styles.</p>
+            {/* Additional information or content for Service 3 */}
+          </div>
+          <div className="service-box">
+            <h2>Drywall Repair</h2>
+            <p>Patching of holes and cracks, repairs on corners and bullnose.</p>
+            {/* Additional information or content for Service 4 */}
+          </div>
+      
+          <div className="service-box">
+            <h2>General Handyman</h2>
+            <p>Don't have the time to tackle those pending tasks around the house? Give me a call!.</p>
+            <p> (480) 331-4840</p>
             {/* Additional information or content for Service 6 */}
           </div>
         </div>
