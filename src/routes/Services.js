8cbfb937b3e4b9a9cfd143c3505bc3927da9cefb
Developer <developer@Rudys-Mac-mini.local>
2023-04-12 15:09:16 -0700

    services layout

--- src/routes/Services.js
+++ src/routes/Services.js
@@ -2,7 +2,38 @@ export default function Services() {
     return (
       <>
         <h1>I Offer the Following Services</h1>
-       
+        <div className="services-grid">
+          <div className="service-box">
+            <h2>Electrical  Repair</h2>
+            <p>Installation and repair of lighting fixtures, ceiling fans, wall outlets and switches.</p>
+            {/* Additional information or content for Service 1 */}
+          </div>
+          <div className="service-box">
+            <h2>Service 2</h2>
+            <p>Description of Service 2</p>
+            {/* Additional information or content for Service 2 */}
+          </div>
+          <div className="service-box">
+            <h2>Service 3</h2>
+            <p>Description of Service 3</p>
+            {/* Additional information or content for Service 3 */}
+          </div>
+          <div className="service-box">
+            <h2>Service 4</h2>
+            <p>Description of Service 4</p>
+            {/* Additional information or content for Service 4 */}
+          </div>
+          <div className="service-box">
+            <h2>Service 5</h2>
+            <p>Description of Service 5</p>
+            {/* Additional information or content for Service 5 */}
+          </div>
+          <div className="service-box">
+            <h2>Service 6</h2>
+            <p>Description of Service 6</p>
+            {/* Additional information or content for Service 6 */}
+          </div>
+        </div>
       </>
     );
   }
