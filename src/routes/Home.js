21daaf953583fde5ae693db94c7e5765e88c4f63
Developer <developer@Rudys-Mac-mini.local>
2023-04-20 15:06:34 -0700

    add request a quote to home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,6 +7,39 @@ export default function Home() {
       <header className="App-header" style={{ backgroundColor: '#f5f5f5' }}>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+
+      <div className="iframe-container">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSd7DCQbJCwkXWqdDwB1ochVizVUlYjSXrbVtaQQ2pErM7pfbw/viewform?embedded=true"
+          width="100%" /* Set width to 100% to fill parent container */
+          height="959" /* Set height to a fixed value */
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          Loading…
+        </iframe>
+      </div>
+
+      <style>
+        {`
+        @media only screen and (min-width: 768px) {
+          .iframe-container {
+            display: none;
+          }
+        }
+
+        @media only screen and (max-width: 767px) {
+          .iframe-container {
+            width: 100%;
+            overflow-x: auto;
+            max-width: 640px;
+            margin: 0 auto;
+          }
+        }
+        `}
+      </style>
+
     </div>
   );
 }
