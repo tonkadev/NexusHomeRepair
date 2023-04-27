51f0f2ca2e9314810b6c8e86a10f2e31284c88e7
Developer <developer@Rudys-Mac-mini.local>
2023-04-27 14:58:41 -0700

    contact form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,10 +1,12 @@
 import logo from '../NexusHomeRepV2.svg';
 import '../Contact.css';
+import { useHistory } from 'react-router-dom';
+
 
 export default function Contact() {
 
   function handleSubmit(event) {
-    event.preventDefault(); // prevent the default form submission behavior
+    event.preventDefault();
     const form = document.getElementById('myForm');
     const formData = new FormData(form);
     const data = {};
@@ -23,48 +25,53 @@ export default function Contact() {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
+        const successMsg = document.getElementById('successMsg');
+        successMsg.style.display = 'block';
+        form.reset();
+        setTimeout(() => {
+          successMsg.style.display = 'none';
+          window.location.href = '/';
+        }, 3000);
       })
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
       });
   }
+  
 
   return (
     <div className="contact-form">
       <form id="myForm" onSubmit={handleSubmit}>
         <label>
-          What type of service are you looking for?
-          <select name="serviceType">
-            <option value="">Select an option</option>
-            <option value="Web Development">Web Development</option>
-            <option value="Mobile App Development">Mobile App Development</option>
-            <option value="UI/UX Design">UI/UX Design</option>
-            <option value="Digital Marketing">Digital Marketing</option>
+          What type of service are you looking for?*
+          <select name="serviceType" required>
+          <option value="General Handyman">General Handyman</option>
+            <option value="Wall Mount Installation">Wall Mount Installation</option>
+            <option value="Furniture Assembly">Furniture Assembly</option>
+            <option value="Trim/Finish Carpentry">Trim/Finish Carpentry</option>
+            <option value="Electrical Fixtures">Electrical Fixtures</option>
+            <option value="Drywall Repair">Drywall Repair</option>
           </select>
         </label>
         <label>
-          Please provide a brief description of the project:
-          <textarea name="description"></textarea>
+          Please provide a brief description of the project:*
+          <textarea name="description" required></textarea>
         </label>
         <label>
-          Name:
-          <input type="text" name="name" />
+          Name:*
+          <input type="text" name="name" required />
         </label>
         <label>
-          Email:
-          <input type="email" name="email" />
+          Email:*
+          <input type="email" name="email" required />
         </label>
         <label>
-          Address:
-          <input type="text" name="address" />
+          Address:*
+          <input type="text" name="address" required />
         </label>
         <label>
-          Phone Number:
-          <input type="tel" name="phone" />
+          Phone Number:*
+          <input type="tel" name="phone" required />
         </label>
         <button type="submit">Submit</button>
       </form>
