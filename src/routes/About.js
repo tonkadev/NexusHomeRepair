be6a7a0dfb1dbca8e18a4356b5a4d8532f0fa602
Developer <developer@Rudys-Mac-mini.local>
2023-05-02 18:53:27 -0700

    edit about message

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,3 +1,5 @@
+import '../About.css';
+
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 import logo from "../myAvatar2.png";
@@ -8,36 +10,34 @@ export default function About() {
     <div className="App">
       <h1>About Me</h1>
 
-      <Avatar
-        alt="profile pic"
-        variant="rounded"
-        src={logo}
-        style={{ width: "325px", height: "360px", margin: "0 auto" }}
-      />
+      <div className="avatar-container">
+        <Avatar
+          alt="profile pic"
+          variant="rounded"
+          src={logo}
+          style={{ width: "325px", height: "360px", margin: "0 auto" }}
+        />
+      </div>
 
-      <p
-        style={{
-          fontFamily: "Arial",
-          fontSize: "26px",
-          color: "blue",
-          lineHeight: "1.5",
-          maxWidth: "800px",
-          margin: "0 auto",
-          padding: "0 16px",
-          marginTop: "20px",
-          marginBottom: "20px"
-        }}
-      >
-        My name is Rudy, I am a licensed contractor providing skilled handyman services to customers in N.E. Mesa and vicinity. 
-        I am committed to providing top-notch service and ensuring that your home improvement and repair projects are completed efficiently and to your satisfaction.
-        I understand the importance of having a reliable handyman who can take care of your maintenance needs,
-        and I am dedicated to providing excellent customer service, timely response, and competitive pricing. 
-        Whether you need help with minor repairs or major renovations, I have the expertise and tools to get the job done right. 
-        I take pride in serving the Mesa community and look forward to working with you. 
-        Please don't hesitate to contact me with any questions or to schedule an appointment. 
-        Thank you for considering my handyman services.
-      </p>
-
+      <div className="text-container">
+        <p className="text">
+          Hi, my name is Rudy. I am a licensed contractor and provider of skilled handyman services to customers in N.E. Mesa and the surrounding area. 
+         
+        </p>
+        <p className="text">
+        
+          I am committed to providing top-notch service and ensuring that your home improvement and repair projects are completed efficiently and to your satisfaction.
+        </p>
+        <p className="text">
+          I understand the importance of having a reliable handyman who can take care of your maintenance needs, and I am dedicated to providing excellent customer service, a timely response, and competitive pricing. 
+          Whether you need help with minor repairs or major renovations, I have the expertise and tools to get the job done right. 
+        </p>
+        <p className="text">
+          I take pride in serving the Mesa community and look forward to working with you. 
+          Please don't hesitate to contact me with any questions or to schedule an appointment. 
+          Thank you for considering my handyman services.
+        </p>
+      </div>
 
       {/* Rest of the code */}
     </div>
