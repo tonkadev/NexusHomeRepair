4b0b86fd419f828b441aafccbb40e69247ba0de2
Developer <developer@Rudys-Mac-mini.local>
2023-07-05 10:00:30 -0700

    Change to about text

--- src/routes/About.js
+++ src/routes/About.js
@@ -21,12 +21,13 @@ export default function About() {
 
       <div className="text-container">
         <p className="text">
-          I am your local handyman in Northeast Mesa with experience in all sorts of home repair and improvement projects. 
-           I enjoy helping people with their household needs and take pride in delivering high-quality workmanship. 
-           Whether it's fixing a leaky faucet, installing new light fixtures, or painting a room, 
-           I have the skills and knowledge to tackle any task efficiently and effectively. 
-           My passion for problem-solving and attention to detail ensure that I can find creative solutions 
-           to any home-related challenge. I am licensed, bonded and Insured so you can rest easy knowing 
+          I am a licensed contractor providing handyman services in Northeast Mesa - 85215 and vicinity.  
+          I have experience in all sorts of home repair and improvement projects. 
+          I enjoy helping people with their household needs and take pride in delivering high-quality workmanship. 
+          My passion for problem-solving and attention to detail ensure that I can find creative solutions 
+           to any home-related challenge. 
+
+           Because I am licensed, bonded and Insured you can rest easy knowing 
            that your home repair and improvement projects are in safe and reliable hands, 
            with added protection against any unforeseen events or liabilities.
 
